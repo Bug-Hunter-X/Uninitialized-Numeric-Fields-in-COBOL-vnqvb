0cb7c664989bb32a76b4fc10498c7ac3385170ce
BugHunter-X <askzenai@gmail.com>
2024-12-29 21:02:05 +0100

    Add bugSolution.cob file

---
+++ bugSolution.cob
@@ -0,0 +1,12 @@
+01  WS-AREA-1 PIC 9(5) VALUE 0. 
+01  WS-AREA-2 PIC 9(5) VALUE 0. 
+
+PROCEDURE DIVISION. 
+    ADD 1 TO WS-AREA-1. 
+    SUBTRACT 1 FROM WS-AREA-2. 
+    IF WS-AREA-1 > WS-AREA-2 THEN 
+        DISPLAY "WS-AREA-1 is greater than WS-AREA-2" 
+    ELSE 
+        DISPLAY "WS-AREA-1 is NOT greater than WS-AREA-2" 
+    END-IF. 
+    STOP RUN.
